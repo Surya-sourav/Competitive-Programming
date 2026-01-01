85327de0bce587cdd24e089027ab883d921aa93a
Surya <surya.aimlx@gmail.com>
2026-01-01 14:46:40 +0530

    Prefix Sum Example

---
+++ c1.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int A[10];
+// Implementing Pre-fix Sum 
+int main()
+{	
+	int n;
+	cout << " Enter The Array Size";
+	cin >> n;
+
+	cout << " Enter The Array Elements";
+	for(int i=1;i<=n;i++)
+	{
+		cin >> A[i];
+	}
+
+
+	cout << " Enter l & r";
+	int l , r ; cin >> l >> r;
+	int sum = 0;
+	//Pre-computed the Sub array values 1-n
+	for(int i=1;i<=n;i++)
+	{
+		sum+=A[i];
+		A[i] = sum;
+	}
+
+	cout << A[r]- A[l-1] << endl;
+
+}
